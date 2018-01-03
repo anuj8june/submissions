614b8c0de1ab52992cae435c0450ae659d144039
anuj8june <anuj8june@gmail.com>
2018-01-03 15:44:12 +0530

    Python part

---
+++ Question_Python/question1_part1.py
@@ -0,0 +1,14 @@
+
+L1 = ['a', 'b', 'c']
+L2 = ['b', 'd']
+
+# For common elements
+for val in L1:
+	if val in L2:
+		print(val)
+
+# For non common elements
+L3 = L1 + L2
+for elem in L3:
+	if (elem not in L1) or (elem not in L2):
+		print(elem)
